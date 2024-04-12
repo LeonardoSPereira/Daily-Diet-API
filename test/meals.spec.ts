a8c8619094d151b271d892c6e8f57c9c3bc82d39
Leonardo Pereira <leonardo_human@hotmail.com>
2024-04-12 14:03:11 -0300

    test: created meals tests

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -1,7 +1,9 @@
 import { afterAll, beforeAll, it, describe, expect } from 'vitest'
 import request from 'supertest'
+import { execSync } from 'node:child_process'
 import { app } from '../src/app'
 import { faker } from '@faker-js/faker'
+import { beforeEach } from 'node:test'
 
 describe('Meals routes', () => {
   beforeAll(async () => {
@@ -34,5 +36,50 @@ describe('Meals routes', () => {
      })
      .expect(201)
 
+  })
+
+  it('should be able to get all meals', async () => {
+    const createUserResponse = await request(app.server)
+     .post('/users')
+     .send({
+        name: faker.person.fullName(),
+        email: faker.internet.email()
+      })
+
+    const cookies = createUserResponse.get("Set-Cookie") || [] // Ensure cookies is an array
+
+    await request(app.server)
+     .get('/meals')
+     .set('Cookie', cookies)
+     .expect(200)
+  })
+
+  it('should be able to get a meal by id', async () => {
+    const createUserResponse = await request(app.server)
+     .post('/users')
+     .send({
+        name: faker.person.fullName(),
+        email: faker.internet.email()
+      })
+
+    const cookies = createUserResponse.get("Set-Cookie") || [] // Ensure cookies is an array
+
+    const createMealResponse = await request(app.server)
+     .post('/meals')
+     .set('Cookie', cookies)
+     .send({
+      name: faker.lorem.words(),
+      description: faker.lorem.text(),
+      is_meal_on_diet: faker.datatype.boolean(),
+      meal_day: faker.lorem.words(),
+      meal_time: faker.lorem.words()
+     })
+
+    const mealId = createMealResponse.body.data.id;
+
+    await request(app.server)
+     .get(`/meals/${mealId}`)
+     .set('Cookie', cookies)
+     .expect(200)
   })
 })
